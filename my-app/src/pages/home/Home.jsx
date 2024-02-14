ba54c60749c5f68f3d8d36be9dd96e18e9aa2a69
vuphuong1794 <phuongvu1794@gmail.com>
2024-02-14 14:07:17 +0700

    test

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">Home</div>
     </div>
   );
 };

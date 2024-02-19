0fc34f64eaadff909d747f9ad5be0b641348ab45
vuphuong1794 <phuongvu1794@gmail.com>
2024-02-19 20:30:51 +0700

    add user list

--- my-app/src/components/datatable/DataTable.jsx
+++ my-app/src/components/datatable/DataTable.jsx
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "user", headerName: "User", width: 200 },
-  { field: "email", headerName: "Email", width: 220 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 130,
-  },
-  { field: "status", headerName: "Status", width: 110 },
-  { field: "action", headerName: "Action", width: 220 },
-];
-
-const rows = [
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+import datatableSource, { userColumns, userRows } from "../../datatableSource";
+import "./dataTable.scss";
+import { Link } from "react-router-dom";
 
 const DataTable = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  const actionColumn = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="cellAction">
+          <Link to="/users/test" style={{ textDecoration: "none" }}>
+            <div className="viewBtn">View</div>
+          </Link>
+          <div
+            className="deleteBtn"
+            onClick={() => handleDelete(params.row.id)}
+          >
+            Delete
+          </div>
+        </div>
+      );
+    },
+  };
   return (
     <div className="datatable">
       <DataGrid
-        rows={rows}
-        columns={columns}
+        rows={data}
+        columns={userColumns.concat(actionColumn)}
         initialState={{
           pagination: {
             paginationModel: { page: 0, pageSize: 5 },
